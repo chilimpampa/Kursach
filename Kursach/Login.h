df598d01264082d92c476677fdc582d7020c8255
tatarin <konovalovdan22072004@mail.ru>
2024-01-19 00:39:18 +0700

    Сделана входная форма

--- Kursach/Login.h
+++ Kursach/Login.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Main_menu.h"
 
 namespace Kursach {
 
@@ -41,12 +42,14 @@ namespace Kursach {
 	private: System::Windows::Forms::PictureBox^ pictureBox3;
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label3;
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -62,7 +65,9 @@ namespace Kursach {
 			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
@@ -71,26 +76,29 @@ namespace Kursach {
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(200, 12);
+			this->pictureBox1->Location = System::Drawing::Point(216, 172);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(70, 66);
+			this->pictureBox1->Size = System::Drawing::Size(78, 71);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(129, 205);
+			this->textBox2->BackColor = System::Drawing::SystemColors::ActiveBorder;
+			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 13.8F));
+			this->textBox2->Location = System::Drawing::Point(122, 356);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(268, 58);
+			this->textBox2->Size = System::Drawing::Size(301, 58);
 			this->textBox2->TabIndex = 2;
 			this->textBox2->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
 			// pictureBox2
 			// 
 			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
-			this->pictureBox2->Location = System::Drawing::Point(44, 205);
+			this->pictureBox2->Location = System::Drawing::Point(37, 356);
 			this->pictureBox2->Name = L"pictureBox2";
 			this->pictureBox2->Size = System::Drawing::Size(79, 58);
 			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
@@ -100,7 +108,7 @@ namespace Kursach {
 			// pictureBox3
 			// 
 			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.Image")));
-			this->pictureBox3->Location = System::Drawing::Point(44, 301);
+			this->pictureBox3->Location = System::Drawing::Point(37, 450);
 			this->pictureBox3->Name = L"pictureBox3";
 			this->pictureBox3->Size = System::Drawing::Size(79, 58);
 			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
@@ -109,10 +117,15 @@ namespace Kursach {
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(129, 301);
+			this->textBox3->BackColor = System::Drawing::SystemColors::ActiveBorder;
+			this->textBox3->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox3->Location = System::Drawing::Point(122, 450);
 			this->textBox3->Multiline = true;
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(268, 58);
+			this->textBox3->PasswordChar = '*';
+			this->textBox3->Size = System::Drawing::Size(301, 58);
 			this->textBox3->TabIndex = 5;
 			this->textBox3->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			// 
@@ -120,44 +133,72 @@ namespace Kursach {
 			// 
 			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 9));
-			this->button1->Location = System::Drawing::Point(167, 451);
+			this->button1->Location = System::Drawing::Point(177, 587);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(120, 42);
 			this->button1->TabIndex = 6;
 			this->button1->Text = L"Войти";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Login::button1_Click);
 			// 
-			// textBox1
+			// label1
 			// 
-			this->textBox1->BackColor = System::Drawing::SystemColors::ControlDarkDark;
-			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 9, System::Drawing::FontStyle::Underline, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox1->ForeColor = System::Drawing::SystemColors::Window;
-			this->textBox1->Location = System::Drawing::Point(139, 121);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->ReadOnly = true;
-			this->textBox1->Size = System::Drawing::Size(184, 37);
-			this->textBox1->TabIndex = 7;
-			this->textBox1->Text = L"Авторизация";
-			this->textBox1->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->label1->ForeColor = System::Drawing::SystemColors::Window;
+			this->label1->Location = System::Drawing::Point(170, 291);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(170, 37);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Авторизация";
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(204)), static_cast<System::Int32>(static_cast<System::Byte>(67)),
+				static_cast<System::Int32>(static_cast<System::Byte>(67)));
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Location = System::Drawing::Point(453, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(39, 37);
+			this->button2->TabIndex = 8;
+			this->button2->Text = L"Х";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Login::button2_Click);
+			// 
+			// label3
+			// 
+			this->label3->Font = (gcnew System::Drawing::Font(L"Sans Serif Collection", 9));
+			this->label3->ForeColor = System::Drawing::SystemColors::Window;
+			this->label3->Location = System::Drawing::Point(81, 68);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(368, 101);
+			this->label3->TabIndex = 10;
+			this->label3->Text = L"Добро пожаловать в Ваш Электронный Кошелек";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// Login
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ControlDarkDark;
-			this->ClientSize = System::Drawing::Size(449, 601);
-			this->Controls->Add(this->textBox1);
+			this->ClientSize = System::Drawing::Size(504, 695);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->pictureBox3);
 			this->Controls->Add(this->pictureBox2);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->pictureBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"Login";
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
@@ -166,5 +207,27 @@ namespace Kursach {
 
 		}
 #pragma endregion
-	};
+		bool password_check(String^ password) {
+			if (password == "262319") {
+				return 1;
+			}
+			else {
+				MessageBox::Show(this, "Пароль неверный!", "Сообщение", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+				return 0;
+			}
+		}
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ password = textBox3->Text;
+		if (password_check(password)) {
+			Main_menu^ main_menu_p = gcnew Main_menu();
+			main_menu_p->Show();
+			main_menu_p->Owner = this;
+			this->Hide();
+		}
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
+};
 }
---
+++ Kursach/Main_menu.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace Kursach {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Main_menu
+	/// </summary>
+	public ref class Main_menu : public System::Windows::Forms::Form
+	{
+	public:
+		Main_menu(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Main_menu()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Main_menu
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1320, 659);
+			this->Name = L"Main_menu";
+			this->Text = L"Main_menu";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
